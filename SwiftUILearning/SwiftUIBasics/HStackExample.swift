78f9dc2630aa7e4c1cd7f9c41e52a3e9e9d1ef75
Berat Yükselen <beratyukselen@gmail.com>
2025-07-03 14:55:01 +0300

    HStack Learned

--- SwiftUILearning/SwiftUIBasics/HStackExample.swift
+++ SwiftUILearning/SwiftUIBasics/HStackExample.swift
@@ -5,3 +5,36 @@
 //  Created by Berat Yükselen on 3.07.2025.
 //
 
+import SwiftUI
+
+struct HStackExample: View {
+    var body: some View {
+        
+        VStack {
+            
+            Text("Hello!")
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            
+            Text("SwiftUI")
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            
+            Text("World")
+                .padding()
+                .background(Color.orange)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    HStackExample()
+}
