75e7be82f91edeadea6e0ea9062c264fb5863f49
Berat Yükselen <beratyukselen@gmail.com>
2025-07-03 14:58:35 +0300

    ZStack Learned

--- SwiftUILearning/SwiftUIBasics/ZStackExample.swift
+++ SwiftUILearning/SwiftUIBasics/ZStackExample.swift
@@ -5,3 +5,28 @@
 //  Created by Berat Yükselen on 3.07.2025.
 //
 
+import SwiftUI
+
+struct ZStackExample: View {
+    var body: some View {
+        
+        ZStack {
+            Color.blue
+                .edgesIgnoringSafeArea(.all)
+            
+            Circle()
+                .fill(Color.green)
+                .frame(width: 200, height: 200)
+                .shadow(radius: 10)
+            
+            Text("ZStack ❤️")
+                .font(.title)
+                .foregroundColor(.white)
+                .bold()
+        }
+    }
+}
+
+#Preview {
+    ZStackExample()
+}
