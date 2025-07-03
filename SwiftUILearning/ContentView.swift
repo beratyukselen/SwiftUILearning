38cdfe02cad2901a0973c04ac0f06ff092b0a024
Berat Yükselen <beratyukselen@gmail.com>
2025-07-03 15:23:18 +0300

    Alignment Learned

---
+++ SwiftUILearning/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  SwiftUILearning
+//
+//  Created by Berat Yükselen on 3.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
--- SwiftUILearning/SwiftUIBasics/AlignmentExample.swift
+++ SwiftUILearning/SwiftUIBasics/AlignmentExample.swift
@@ -5,3 +5,83 @@
 //  Created by Berat Yükselen on 3.07.2025.
 //
 
+//
+//  Alignment.swift
+//  SwiftUIDemoApp
+//
+//  Created by Berat Yükselen on 3.07.2025.
+//
+
+import SwiftUI
+
+struct AlignmentExample: View {
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+             Text("Merhaba SwiftUI!")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Button {
+                    print("Tıklandı")
+                } label: {
+                    Text("Tıkla")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+            
+            .padding()
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(10)
+            
+            VStack(alignment: .center) {
+                Text("Merhaba Swift!")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Button {
+                    print("Tıklandı")
+                } label: {
+                    Text("Tıkla")
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+            
+            .padding()
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(10)
+            
+            VStack(alignment: .trailing) {
+             Text("Merhaba SwiftUI!")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Button {
+                    print("Tıklandı")
+                } label: {
+                    Text("Tıkla")
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+            
+            .padding()
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(10)
+            
+        }
+    }
+}
+
+#Preview {
+    AlignmentExample()
+}
+
