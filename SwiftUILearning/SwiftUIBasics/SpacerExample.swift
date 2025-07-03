9f8dc6c4bc6c29b7cc2f780770b4320dc2f22e9f
Berat Yükselen <beratyukselen@gmail.com>
2025-07-03 15:35:20 +0300

    Spacer Learned

--- SwiftUILearning/SwiftUIBasics/SpacerExample.swift
+++ SwiftUILearning/SwiftUIBasics/SpacerExample.swift
@@ -5,3 +5,22 @@
 //  Created by Berat Yükselen on 3.07.2025.
 //
 
+import SwiftUI
+
+struct SpacerExample: View {
+    var body: some View {
+        VStack {
+            
+            Text("First Text")
+            
+            Spacer()
+            
+            Text("Second Text")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SpacerExample()
+}
