ddd068e9e227d87f7d2d1dcf8ac8a6a8cfbc8e0c
Berat Yükselen <beratyukselen@gmail.com>
2025-07-03 15:45:07 +0300

    ImageLearned

--- SwiftUILearning/SwiftUIBasics/ImageExample.swift
+++ SwiftUILearning/SwiftUIBasics/ImageExample.swift
@@ -5,3 +5,17 @@
 //  Created by Berat Yükselen on 3.07.2025.
 //
 
+import SwiftUI
+
+struct ImageExample: View {
+    var body: some View {
+        
+        Image("vikings")
+            .resizable()
+            .frame(width: 400, height: 250)
+    }
+}
+
+#Preview {
+    ImageExample()
+}
