014793ebd21ff43d43e35faa22187968bab6ec0c
Berat Yükselen <beratyukselen@gmail.com>
2025-07-03 15:39:16 +0300

    Frame Learned

--- SwiftUILearning/SwiftUIBasics/FrameExample.swift
+++ SwiftUILearning/SwiftUIBasics/FrameExample.swift
@@ -5,3 +5,27 @@
 //  Created by Berat Yükselen on 3.07.2025.
 //
 
+import SwiftUI
+
+struct FrameExample: View {
+    var body: some View {
+        VStack {
+            
+            Text("Merhaba Swift")
+                .frame(width: 200, height: 50)
+                .background(Color.green)
+            
+            Spacer()
+                .frame(height: 50)
+            
+            Text("Hoş Geldiniz")
+                .frame(width: 200, height: 50)
+                .background(Color.blue)
+        }
+    }
+}
+
+#Preview {
+    FrameExample()
+}
+
